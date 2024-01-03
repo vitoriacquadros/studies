f804a0cef5625e51ded6e8673386c368bac17f0a
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-01-03 17:45:49 -0300

    Variable
    Criação básica de variáveis em Python

---
+++ variaveis.py
@@ -0,0 +1,24 @@
+# variaveis são utilizadas para salvar algo na memória do computador
+# PEP8: inicie variaveis com letras minúsculas, pode usar números ou underline _.
+# o sinalde '=' é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variável)
+# Uso: nome_variavel = expressão snakecase 
+# o nome da variavel precisa ser descritivo, explicando exatamente o que é que está no valor atribuido
+nome_completo = 'Ada'
+soma_de_dois = 1 +3 
+int_1 = int('1') + 6
+print(nome_completo)
+print(soma_de_dois)
+
+print(nome_completo, soma_de_dois)
+print(int_1)
+
+nome = 'Luiz'
+idade = 15
+maior_de_idade = idade >= 18 
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('É maior?', maior_de_idade)
+
+
+
