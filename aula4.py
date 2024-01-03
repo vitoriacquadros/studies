d2ef2aa9cc5b4ac7811364cf77542fffc6e435af
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-01-03 17:44:54 -0300

    Convertion
    Conversão em Python entre tipos de dados;

---
+++ aula4.py
@@ -0,0 +1,16 @@
+# conversão de tipos, coerção 
+# type convertion, typecasting, coercion é o ato de converter um tipo em outro tipos imutáveis e primitivos:
+# str, int, float, bool
+# polimorfismo usa o mesmo operador para juntar duas coisas, só acontece em linguagens dinâmicas
+
+
+# print(1 + 1)
+
+# converter um tipo em outro se puder ser feito
+
+print(int('1'), type(int('1')))
+print(int('1') + 1) #altera o valor de str para int
+#pode usar float para converter para soma de um numero float e inteiro 
+
+print(bool('     '))
+print(11)
