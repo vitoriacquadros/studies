b9d645e5705868e02cf2a3de90b3849fc32c1838
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-01-03 16:51:40 -0300

    Types str, int and float
    Aulas introdutórias sobre o tipo str, int e float abordando suas utilidades básicas e algumas funções agregadas para tratamento de dados em Python

---
+++ aula1.py
@@ -0,0 +1,16 @@
+print("Oi")
+print("Aleatório")
+
+print(12,14)
+print(12,54)
+
+
+print(44, 45, sep = " -- ")
+
+print("Programar \"é bom\"")
+
+print(r"Programar \"é bom\"")
+
+print('Programar "é bom"')
+
+print('Explícito', 'é', 'melhor " do que implícito')
---
+++ aula2.py
@@ -0,0 +1,7 @@
+print(11)
+
+print(0)
+
+a = 16
+
+print(a)
