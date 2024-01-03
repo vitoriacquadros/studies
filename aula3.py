e71448f631f57f4b64707bb507683f6563161fc2
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-01-03 17:44:34 -0300

    Type bool
    Descrição básica sobre o tipo booleano.

---
+++ aula3.py
@@ -0,0 +1,14 @@
+# Tipo bool 
+#Quando algo é questionado em um programada, só existem duas respostas possíveis
+# sim(True) ou não(False)
+# Existem vários operadores para "questionar"
+#Dentre eles o ==, que é um operador lógico que questiona se um valor é igual ao outro
+
+# print(10 == 10 and 12 == 10)
+
+print(10 == 10) # Sim -> True(verdadeiro)
+print(11 == 10) # Não -> False(falso) 
+
+print(type(10 == 10))
+
+# um string vazia ('') é considerada False e uma string com conteúdo é considerada True (' ')
