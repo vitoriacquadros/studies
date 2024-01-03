f8bf813f69b988a7a0199337ad3c0e8a9ad08d64
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-01-03 17:45:31 -0300

    Exercício 1
    Exercício sobre o uso básico de dados na linguagem Python criando variáveis para serem impressas quando a função print() for chamada.

---
+++ exercicio1.py
@@ -0,0 +1,17 @@
+nome = 'Ada'
+sobrenome = 'Lovelace'
+idade = 209
+ano_atual = 2024
+ano_nascimento = ano_atual - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.60
+
+
+
+print('Nome:', nome) #str
+print('Sobrenome:', sobrenome) #str
+print('Idade:', idade) #int
+print('Ano de nascimento:', ano_nascimento) #int
+print('É maior de idade? ', maior_de_idade) #bool
+print('Altura em metros:  ', altura_metros) #float
+
