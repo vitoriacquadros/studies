ad81c7e17a561910c99a01a3aa512dc3e29596d7
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-01-13 11:24:29 -0300

    Format
    Métodos de formatação de strings

---
+++ format.py
@@ -0,0 +1,12 @@
+# Formas de formatação de string
+
+a = 'A'
+b = 'B'
+c = 1.1
+string = 'a ={0} b= {1} c= {2 }'
+formato = string.format(a, b, c)
+
+print(formato)
+
+# format é uma função que está dentro de um objeto chamado string
+
