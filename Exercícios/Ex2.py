7fb07ebce4d186392d7aac6ebdcf9c1844d15989
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-01-13 11:24:48 -0300

    Cálculo IMC
    Cálculo simples de IMC

---
+++ Exercícios/Ex2.py
@@ -0,0 +1,18 @@
+#cálculo do IMC 
+
+nome: str = 'Gustavo'
+
+idade: int = 18
+
+peso: int = 70
+
+altura: float = 1.80 
+
+IMC = peso / altura*2 
+
+print(f"{nome} tem {idade} anos e seu IMC é igual a {IMC:.2f}")
+
+
+
+
+
