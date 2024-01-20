fcec8eeeda4426f7bd8402c1b3ea9975c610b0e9
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-01-19 21:49:16 -0300

    Create fatiamentodestrings.py

---
+++ fatiamentodestrings.py
@@ -0,0 +1,13 @@
+'''
+Fatiamento de string 
+
+012345678
+Olá mundo 
+-987654321
+Fatiamento [i:f:p] [::]
+obs.: a função len retorna quantidade de caracteres na string
+'''
+
+v = 'Olá mundo'
+
+print(v[4])
