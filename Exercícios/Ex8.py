3a941e38e55638e61a7bcfd1b6921a0f03d59c00
Vitória Quadros <vitoriaquadrosc13@outlook.com>
2024-02-09 10:12:58 -0300

    Create Ex8.py

---
+++ Exercícios/Ex8.py
@@ -0,0 +1,6 @@
+# EX.:Exiba os indices da lista
+
+lista = ['Maria', 'João', 'José', 'Antônio', 'Joana', 'Francisco', 'Ana', 'Luiz', 'Paulo', 'Carlos']
+
+for i in range(len(lista)):
+    print(i, lista[i])
