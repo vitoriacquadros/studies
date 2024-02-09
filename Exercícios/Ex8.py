b4992ce57e437b75e64bcd9a9b8979ed5a88efcd
Vitória Quadros <vitoriaquadrosc13@outlook.com>
2024-02-09 16:03:32 -0300

    Update Ex8.py

--- Exercícios/Ex8.py
+++ Exercícios/Ex8.py
@@ -3,4 +3,4 @@
 lista = ['Maria', 'João', 'José', 'Antônio', 'Joana', 'Francisco', 'Ana', 'Luiz', 'Paulo', 'Carlos']
 
 for i in range(len(lista)):
-    print(i, lista[i])
+    print(i, lista[i], type(lista))
