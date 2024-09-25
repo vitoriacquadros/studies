21b61ed27f810fd32abb220a66258ea0a1279a63
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-09-25 15:17:33 -0300

    update

--- desempacotamento_em_chamadas.py
+++ desempacotamento_em_chamadas.py
@@ -1,2 +1,9 @@
 # Desempacotamento em chamdas de métodos e funções
 
+string = 'Python'
+
+lista = ['Yara', 'Python', 'Java']  
+
+tupla = 'Python', 'Java', 'C#'
+
+print(*lista)  # Yara Python Java
